d209edd86c48e9e06fc7c83aabac1aee332ed499
Yorisca7 <yorisca77janarti@gmail.com>
2025-06-21 02:34:51 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yorisca.skzshop',
+  appName: 'straykids-eshop',
+  webDir: 'www'
+};
+
+export default config;
